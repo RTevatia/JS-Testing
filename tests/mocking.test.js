b98d0f8347f6fefb96768ef18501a94a0685a859
Rahul Tevatia <rtevatia2@gmail.com>
2025-10-30 18:07:36 -0400

    Mock function example - sendText

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -2,11 +2,18 @@ import { vi, it, expect, describe } from "vitest";
 
 describe('test suite', () => {
     it('test case', () => {
-        const greet = vi.fn();
-        greet.mockImplementation(name => 'Hello ' + name);
+        // Create a mock for the following function
+        const sendText = vi.fn();
+        sendText.mockReturnValue('ok');
 
-        const result = greet('Mosh');
+        // sendText(message) {}
+        // Call the mock function
+        const result = sendText('message');
         
-        expect(greet).toHaveBeenCalledOnce();
+        // Assert that the mock function is called
+        expect(sendText).toHaveBeenCalledWith('message');
+        // Assert that the result is 'ok'
+        expect(result).toBe('ok');
     })
+    
 })
