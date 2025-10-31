7b950f5dfc597264ef3847491423104eb881340b
Rahul Tevatia <rtevatia2@gmail.com>
2025-10-31 05:59:01 -0400

    Testing submitOrder

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,13 +1,15 @@
 import { vi, it, expect, describe } from "vitest";
-import { getPriceInCurrency, getShippingInfo, renderPage } from "../src/mocking";
+import { getPriceInCurrency, getShippingInfo, renderPage, submitOrder } from "../src/mocking";
 import { getExchangeRate } from "../src/libs/currency";
 import { getShippingQuote } from "../src/libs/shipping";
 import { trackPageView } from "../src/libs/analytics";
+import { charge } from "../src/libs/payment";
 
 // Import mocking module
-vi.mock('../src/libs/currency')
-vi.mock('../src/libs/shipping')
-vi.mock('../src/libs/analytics')
+vi.mock('../src/libs/currency');
+vi.mock('../src/libs/shipping');
+vi.mock('../src/libs/analytics');
+vi.mock('../src/libs/payment');
 
 describe('test suite', () => {
     it('test case', () => {
@@ -69,4 +71,34 @@ describe('render page', () => {
 
         expect(trackPageView).toHaveBeenCalledWith('/home');
     })
+})
+
+
+describe('submitOrder', () => {
+    const order = { totalAmount: 10 };
+    const creditCard = { creditCardNumber: '1234' };
+
+    it('should charge the customer', async () => {
+        vi.mocked(charge).mockReturnValue({ status: 'success' });
+
+        await submitOrder(order, creditCard);
+
+        expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount);
+    });
+
+    it('should return success when payment is successful', async () => {
+        vi.mocked(charge).mockReturnValue({ status: 'success' });
+
+        const result = await submitOrder(order, creditCard);
+
+        expect(result).toEqual({ success: true });
+    });
+
+    it('should return error when payment fails', async () => {
+        vi.mocked(charge).mockReturnValue({ status: 'failed' });
+
+        const result = await submitOrder(order, creditCard);
+
+        expect(result).toEqual({ success: false, error: 'payment_error' });
+    });
 })
